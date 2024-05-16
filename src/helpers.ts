62a8e2548b9067f00ec3a9e2e3fa7f2423d0d59f
rajcantcode <mahadeshwarraj23@gmail.com>
2024-05-16 08:46:55 +0530

    chore: Refactor authenticateToken function to return user name instead of a boolean

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 import { User } from "./model/User.js";
 import redis from "./lib/database/redis.js";
 
-export const authenticateToken = async (token: string): Promise<boolean> => {
+export const authenticateToken = async (
+  token: string
+): Promise<string | null> => {
   const secret = process.env.ACCESS_TOKEN_SECRET;
   if (!secret) {
     throw new Error("ACCESS_TOKEN_SECRET is not set");
@@ -10,18 +12,11 @@ export const authenticateToken = async (token: string): Promise<boolean> => {
   try {
     const payload = await jwt.verify(token, secret);
     if (typeof payload === "object" && "name" in payload) {
-      const user =
-        (await redis?.hgetall(
-          `${payload.guest ? `guest` : `user`}:${payload.name}`
-        )) || (await User.findOne({ username: payload.name }));
-      if (!user) {
-        return false;
-      }
-      return true;
+      return payload.name;
     }
-    return false;
+    return null;
   } catch (error) {
     console.log(error);
-    return false;
+    return null;
   }
 };
